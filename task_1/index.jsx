1ffa75ed71f1a51fbae49ef8c9221f2bdbc3d9ad
Gleb Demichshuk <77880315+Isca7el@users.noreply.github.com>
2022-04-08 13:34:27 +0600

    feat: 1 task

--- task_1/index.jsx
+++ task_1/index.jsx
@@ -1,15 +1,24 @@
 import { Component } from 'react';
 
 // functional component
-const FirstComponent = ({ name }) => (
+const FirstComponent = React.memo(({ name }) => (
     <div>my name is {name}</div>
-);
+));
 
 // class component
 class SecondComponent extends Component {
+    shouldComponentUpdate(prevProps, nextProps) {
+        if (prevProps.name === nextProps.name){
+            return false
+        }
+    }
     render() {
         return (
             <div>my name is {this.props.name}</div>
         )
     }
 }
+
+{/* При решении данной задачи на классовых компонентах так, же можн использовать React.PureComponent,
+оиличе его от React.Component, тем что React.PureComponent реализует метод shouldComponentUpdate(),
+т.е если будет вложенность, либо сложная структура, будт происходить ренлеринг */}
