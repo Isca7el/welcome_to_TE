0198f5b1ff8dda74275d4794962e73780367d7ad
Gleb Demichshuk <77880315+Isca7el@users.noreply.github.com>
2022-04-10 15:08:38 +0600

    feat: 7 task

--- task_7/index.jsx
+++ task_7/index.jsx
@@ -1,31 +1,37 @@
 import { useState } from 'react';
+const withBlock = (Сomponent) => {
+    return (props) => {
+        const [ isActive, setActive ] = useState(false);
+
+        const mouseEnterHandler = () => {
+            setActive(true);
+            mouseEnterCallbak();
+        }
+
+        return <Component 
+        {...props}
+        isActive={isActive}
+         setActive={setActive}/>
+    }
+}
 
 const Block1 = ({ mouseEnterCallbak, imgSrc, imgAlt }) => {
-    const [ isActive, setActive ] = useState(false);
-
-    const mouseEnterHandler = () => {
-        setActive(true);
-        mouseEnterCallbak();
-    }
 
     return (
-        <div onMouseEnter={mouseEnterHandler} className={ isActive ? 'active': '' }>
+        <div onMouseEnter={mouseEnterHandler} className={ props.isActive ? 'active': '' }>
             <img src={imgSrc} alt={imgAlt} />
         </div>
     );
 }
 
 const Block2 = ({ mouseEnterCallbak, content }) => {
-    const [ isActive, setActive ] = useState(false);
-
-    const mouseEnterHandler = () => {
-        setActive(true);
-        mouseEnterCallbak();
-    }
 
     return (
-        <div onMouseEnter={mouseEnterHandler} className={ isActive ? 'active': '' }>
+        <div onMouseEnter={mouseEnterHandler} className={ props.isActive ? 'active': '' }>
             <p>BLock2 content: {content}</p>
         </div>
     );
 }
+
+const Block1withBlock = withBlock(Block1);
+const Block2withBlock = withBlock(Block2);
